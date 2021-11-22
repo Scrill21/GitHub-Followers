f26079c4da4541f78d342d217b6a9e9bd5fd3a4b
Anthony Byrd <scrill21@gmail.com>
2021-11-21 19:27:22 -0500

    Created file structure, navigation and tabbar controllers, search and favoritesList view controllers, and configured programmatic constraints

---
+++ GitHubFollowers/GitHubFollowers/Controllers/View Controllers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by anthony byrd on 11/21/21.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}//End of class
