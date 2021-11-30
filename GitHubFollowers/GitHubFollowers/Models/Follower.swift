03a355cfde38d98947eb8b63fdaa2caf84c2eb2c
Anthony Byrd <scrill21@gmail.com>
2021-11-30 00:21:09 -0500

    Added user and followers models

---
+++ GitHubFollowers/GitHubFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by anthony byrd on 11/29/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
