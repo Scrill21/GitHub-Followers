8d89c6e7eba86fc659caf2094f55c31ad1f05870
Anthony Byrd <scrill21@gmail.com>
2021-12-21 00:00:02 -0500

    Updated file structure, created custom cell and avatarImageView, and started configuring UICollectionView in FollowersListVC

--- GitHubFollowers/GitHubFollowers/Controllers/View Controllers/FollowerListVC.swift
+++ GitHubFollowers/GitHubFollowers/Controllers/View Controllers/FollowerListVC.swift
@@ -10,12 +10,14 @@ import UIKit
 class FollowerListVC: UIViewController {
     //MARK: - Properties
     var username: String!
+    var collectionView: UICollectionView!
     
     //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -24,6 +26,27 @@ class FollowerListVC: UIViewController {
     }
     
     //MARK: - Methods
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
     
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollower(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.localizedDescription, buttonTitle: "Ok")
+            }
+        }
+    }
 
 }//End of class
