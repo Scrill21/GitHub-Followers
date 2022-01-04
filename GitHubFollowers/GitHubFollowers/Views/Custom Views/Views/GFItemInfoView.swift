ed1f0c5f392b2bdc273f5440cb4b6472df11b131
Anthony Byrd <scrill21@gmail.com>
2022-01-03 23:44:50 -0500

    Created GFItemInfoView

---
+++ GitHubFollowers/GitHubFollowers/Views/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  GitHubFollowers
+//
+//  Created by anthony byrd on 1/3/22.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
